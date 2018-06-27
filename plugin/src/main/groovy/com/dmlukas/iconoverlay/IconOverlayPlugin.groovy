16c66ecf14d05419bd452e57fcdccfd0eb342c72
Dzmitry <dmlukas@gmail.com>
2018-06-28 00:07:48 +0300

    publish plugin

--- plugin/src/main/groovy/com/dmlukas/iconoverlay/IconOverlayPlugin.groovy
+++ plugin/src/main/groovy/com/dmlukas/iconoverlay/IconOverlayPlugin.groovy
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nobleworks_software
+package com.dmlukas.iconoverlay
 
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.api.BaseVariant
